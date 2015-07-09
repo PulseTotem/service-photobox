0b3a847c0358fe2f1f63a0f3873a974f8985157b
Simon Urli <simon.urli@gmail.com>
2015-07-09 16:24:14 +0200

    SERVICES-53 Continue to create service photobox

--- scripts/PhotoboxNamespaceManager.ts
+++ scripts/PhotoboxNamespaceManager.ts
@@ -4,10 +4,14 @@
  */
 
 /// <reference path="../t6s-core/core-backend/scripts/server/SourceNamespaceManager.ts" />
-
-/// <reference path="./sources/CloudConnectePhotobox.ts" />
+/// <reference path="../t6s-core/core-backend/t6s-core/core/scripts/infotype/CmdList.ts" />
+/// <reference path="../t6s-core/core-backend/t6s-core/core/scripts/infotype/Cmd.ts" />
+/// <reference path="../t6s-core/core-backend/scripts/Logger.ts" />
 
 class PhotoboxNamespaceManager extends SourceNamespaceManager {
+
+	private _params : any;
+	private _cmdSession : Cmd;
 
 	/**
 	 * Constructor.
@@ -17,6 +21,57 @@ class PhotoboxNamespaceManager extends SourceNamespaceManager {
 	 */
 	constructor(socket : any) {
 		super(socket);
-		this.addListenerToSocket('CloudConnectePhotobox', function(params : any, self : PhotoboxNamespaceManager) { (new CloudConnectePhotobox(params, self)) });
+		this.addListenerToSocket('subscribe', this.subscribe);
+	}
+
+	/**
+	 * Subscribe to notifications.
+	 *
+	 * @method subscribe
+	 * @param {Object} params - Params to subscribe to notifications : ???.
+	 * @param {NotifierNamespaceManager} self - the NotifierNamespaceManager's instance.
+	 */
+	subscribe(params : any, self : PhotoboxNamespaceManager = null) {
+		if(self == null) {
+			self = this;
+		}
+
+		Logger.debug("listenNotifications Action with params :");
+		Logger.debug(params);
+		self._params = params;
+	}
+
+	/**
+	 * Method called when external message come (from API Endpoints for example).
+	 *
+	 * @method onExternalMessage
+	 * @param {string} from - Source description of message
+	 * @param {any} message - Received message
+	 */
+	onExternalMessage(from : string, message : any) {
+		if (from == "startSession") {
+			var cmdList : CmdList = new CmdList(uuid.v1());
+			var cmd : Cmd = new Cmd(uuid.v1());
+			cmd.setCmd("startSession");
+			cmd.setPriority(InfoPriority.HIGH);
+			cmd.setDurationToDisplay(30000);
+			cmdList.addCmd(cmd);
+			this._cmdSession = cmd;
+
+			this.sendNewInfoToClient(cmdList);
+		} else if (from == "validPicture") {
+			this._cmdSession.setDurationToDisplay(0);
+
+			var cmd : Cmd = new Cmd(uuid.v1());
+			cmd.setCmd("validatedPicture");
+			cmd.setPriority(InfoPriority.HIGH);
+			cmd.setDurationToDisplay(10);
+
+			var cmdList : CmdList = new CmdList(uuid.v1());
+			cmdList.addCmd(cmd);
+			cmdList.addCmd(this._cmdSession);
+
+			this.sendNewInfoToClient(cmdList);
+		}
 	}
 }

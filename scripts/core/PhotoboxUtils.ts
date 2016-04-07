2e9ba1d8467e6f30441d1b9d0315229ea8a3fcc4
Simon Urli <simon.urli@gmail.com>
2016-04-07 17:06:07 +0200

    SERVICES-151 Next trial with interpolation "moving-average"

--- scripts/core/PhotoboxUtils.ts
+++ scripts/core/PhotoboxUtils.ts
@@ -164,9 +164,8 @@ class PhotoboxUtils {
 													}
 												}
 
-												// Use "grid" interpolation as recommended in https://github.com/aslansky/css-sprite/issues/42#issuecomment-76169341
-												// in order to solve http://jira.the6thscreen.fr/browse/SERVICES-151
-												logoLeft.batch().resize(newLogoLeftWidth, newLogoLeftHeight, "cubic")
+												// Try interpolation to solve http://jira.the6thscreen.fr/browse/SERVICES-151
+												logoLeft.batch().resize(newLogoLeftWidth, newLogoLeftHeight, "moving-average")
 													.writeFile(localLogoLeft, leftExtension, function (errWriteLogoLeft) {
 														if (errWriteLogoLeft) {
 															failCallback("Error when resizing logo left: "+JSON.stringify(errWriteLogoLeft));

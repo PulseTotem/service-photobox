97bee92e0237b6ffeb4c6f7a3694f5720c099c9c
Simon Urli <simon.urli@gmail.com>
2016-05-24 09:22:44 +0200

    INTERACT-15 Try with medium picture...

--- scripts/PhotoboxNamespaceManager.ts
+++ scripts/PhotoboxNamespaceManager.ts
@@ -209,7 +209,7 @@ class PhotoboxNamespaceManager extends SessionSourceNamespaceManager {
 				Logger.debug("Oauth OK for tweeting");
 
 				var form : any = new formData();
-				form.append('media', request(picture.getURLOriginalPicture()));
+				form.append('media', request(picture.getURLMediumPicture()));
 
 				var urlUploadPic = "https://upload.twitter.com/1.1/media/upload.json";
 
